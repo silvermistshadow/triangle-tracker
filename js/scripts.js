481a48e47d844a98de5e59c9c82527ea0d5d36e3
silvermistshadow <bmead72@gmail.com>
2019-03-04 13:32:00 -0800

    keep writing html and javascript

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,18 @@
 //NOT a triangle: The sum of the lengths of any two sides of a triangle is less than or equal to the length of the third side.
 
 var triangleType = function(side1, side2, side3) {
-  
+  if ((side1 == side2) && (side2 == side3)) {
+      return "Equilateral"
+  }
+  else if ((side1 == side2)^(side2 == side3)) {
+      return "Isosceles"
+  }
+  else if ((side1 == side3) && (side2 != side3)) {
+    return "Isosceles"
+  }
+  else if () {
 
+  }
 
 };
 
@@ -21,6 +31,13 @@ $(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
     var side1 = parseInt($("#side1").val());
+    var side2 = parseInt($("#side2").val());
+    var side3 = parseInt($("#side3").val());
+
+    var triangle = triangleType (side1, side2, side3)
+
+    $("#output").text(triangle)
+
 
   });
 
