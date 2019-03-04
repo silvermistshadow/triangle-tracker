82226e712112dd1e769500c26f2f6af5412713c5
silvermistshadow <bmead72@gmail.com>
2019-03-04 12:09:31 -0800

    start writing html and javascript

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+// Back-end logic
+//Equilateral: All sides are equal.
+//Isosceles: Exactly 2 sides are equal.
+//Scalene: No sides are equal.
+//NOT a triangle: The sum of the lengths of any two sides of a triangle is less than or equal to the length of the third side.
+
+var triangleType = function(side1, side2, side3) {
+  
+
+
+};
+
+
+
+
+
+
+// User interface beyond this point
+
+$(document).ready(function(){
+  $("#formOne").submit(function(event){
+    event.preventDefault();
+    var side1 = parseInt($("#side1").val());
+
+  });
+
+
+});
